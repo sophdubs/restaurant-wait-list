9f7b99fca4946cb0826cffe0bacaffb06a184495
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-04-09 20:28:49 +1000

    cleaned up some old code that was commented out

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const Guest = require('../models/Guest');
 
 
 router.get('/', (req, res) => {
-
     var guests;
     Guest.find({}, function(err, docs){
         if (err) throw err;
@@ -18,28 +17,5 @@ router.get('/', (req, res) => {
         return res.render('Welcome', {waitTime});
     });
 });
-
-
-
-
-
-
-
-//Welcome page
-// router.get('/', (req, res) => {
-//     var guests;
-//     Guest.find({}, function(err, docs){
-//         console.log('after find but before guests print');
-//         if (err) throw err;
-//         guests = docs;
-//         if (typeof guests != 'undefined') {
-//             var waitTime = guests.length;
-//             return render('Welcome', {waitTime});
-//         } else {
-//             var waitTime = 5;
-//             res.render('Welcome', {waitTime});
-//         }
-
-
 
 module.exports = router;
